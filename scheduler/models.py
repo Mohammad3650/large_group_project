5c9f59eee5cb9e9b6e5958774ea2cf73257b5369
Shahmeer <shahmeerkhalid5890@gmail.com>
2026-02-10 13:53:50 +0000

    implement event model

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE) #TODO: Check if correct user model used
+    name = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    recurring = models.BooleanField(default=False) 
+
