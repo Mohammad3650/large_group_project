7f8539e779d0c0770c0c2a82926e7a7ead4846c6
“Nabil <nabil.ahmed@kcl.ac.uk>
2026-02-23 14:52:54 +0000

    Create a react page for when the user submits a successful time block

--- frontend/src/components/successfulTimeBlock.jsx
+++ frontend/src/components/successfulTimeBlock.jsx
@@ -1,4 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import "../SuccessPageStyle.css";
+import tickImage from "../functionality/landingpage/photos/tick.png";
+
+
+
 function SuccessfulTimeBlock(){
-    return <h2>Successful!!!!!</h2>;
+    const nav = useNavigate();
+
+    return(
+        <>
+            <div className="success-page">
+                <div className="succes-card">
+                    <h1 className="success-text">Time Block Created Successfully</h1>
+
+                    <div className="tick-image">
+                        <img className="photo" src={tickImage}></img>
+                    </div>
+
+                    <h3>Your time block was created successfully. You can either create a new time block or return to your dashboard.</h3>
+                
+                    <div className="success-btns">
+                        <button
+                        className="go-timeblock-btn"
+                        onClick={() => nav("/create-schedule")}
+                        >
+                            New Time Block
+                        </button>
+            
+                        <button
+                        className="go-dashboard-btn"
+                        onClick={() => nav("/dashboard")}
+                        >
+                            Return To Dashboard
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
 }
 export default SuccessfulTimeBlock;
