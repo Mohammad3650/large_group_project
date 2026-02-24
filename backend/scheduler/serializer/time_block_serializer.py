602997f54c6f7f8bbf9295326e5b96e0f5845c9d
Iyaad-06 <iyaadislam19@gmail.com>
2026-02-24 16:45:02 +0000

    Add description field to TimeBlock; support description in save endpoint; enforce unique DayPlan per user/date; add UK date input support

--- backend/scheduler/serializer/time_block_serializer.py
+++ backend/scheduler/serializer/time_block_serializer.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from ..models import DayPlan, TimeBlock
 
 class TimeBlockSerializer(serializers.Serializer):
-    date = serializers.DateField()
+    date = serializers.DateField(input_formats=["%Y-%m-%d", "%d/%m/%Y"])
     start_time = serializers.TimeField()
     end_time = serializers.TimeField()
-    location = serializers.CharField(allow_blank=True, required=False)
+    location = serializers.CharField(allow_blank=True, required=False, default="")
+    description = serializers.CharField(allow_blank=True, required=False, default="")
     block_type = serializers.ChoiceField(choices=TimeBlock.BLOCK_TYPE_CHOICES)
 
     def validate(self, attrs):
