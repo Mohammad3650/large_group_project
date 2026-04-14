28e6fb19b2f2c4efb2ff8047989b16b4bf9c8610
Hamza Khan <hamza.4.khan@kcl.ac.uk>
2026-04-14 16:15:28 +0100

    Renamed capitalise function to be lower case to be consistent with names of util files

--- frontend/src/utils/Formatters/capitalise.js
+++ frontend/src/utils/Formatters/capitalise.js
@@ -4,8 +4,8 @@
  * @param {string} string - The string to capitalise
  * @returns {string} The string with its first character capitalised
  */
-function Capitalise(string) {
+function capitalise(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export default Capitalise;
+export default capitalise;
